e3b449509446eed55b2a348d6671dd76d3fecda9
KaliFox20 <Daniel.Shiraishi@gmail.com>
2022-08-23 12:37:52 -1000

    added more to app.js and env

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+      cityLat: 0,
+      cityLon: 0,
+    }
+  }
+
+handleInput = (e) => {
+  e.preventDefault();
+  this.setState({
+    city: e.target.value
+  })
 }
 
-export default App;
+getCityData = async (e) => {
+  e.preventDefault();
+
+ try{
+  let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+
+  let cityData = await axios.get(url);
+
+  this.setState({
+    cityLat: cityData.data[0].lat,
+    cityLon: cityData.data[0].lon,
+  })
+
+
+  console.log(cityData.data[0]);
+ } catch(error){
+  this.setState({
+    errorMessage: error.message
+  })
+ }
+}
+
+render (){
+  return (
+    <div>
+      <h1>City Explorer</h1>
+      <form onSubmit={this.getCityData}>
+        <label> Select of City
+          <input type="text" onInput ={this.handleInput} />
+        </label>
+    <button type='submit'>Explore!</button>
+      </form>
+    </div>
+  )
+}
+
